feb614ceeb693d0330bd841f8c3c8f0a251839be
yuhao <983463668@qq.com>
2020-12-13 23:49:27 +0800

    第一此idea提交

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class DemoController {
+
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String getHello() {
+
+        ArrayList<Object> arrayList = new ArrayList<>();
+
+
+
+        Map<String, Object> stringObjectMap = new HashMap<>();
+
+        return "hello idea!!!";
+    }
+}
